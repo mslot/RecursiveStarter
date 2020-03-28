1a57277868796644d52e25e42f45f43e3b8bdeef
Martin Slot <mas@impact.dk>
2020-03-28 11:33:53 +0100

    Added load of settings file

--- src/RecursiveStarter.py
+++ src/RecursiveStarter.py
@@ -1,5 +1,12 @@
+import toml
 import glob
 import os
+
+settings = None
+
+with open("config/settings.toml", encoding="utf-8") as settings_file:
+    content = settings_file.read()
+    settings = toml.loads(content)
 
 sln_files = glob.glob("./**/*.sln",recursive=True)
 
